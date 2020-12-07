668557d1b43f7b1b80ba48586e0c40187521e6dc
henry <24dyckh@lynden.wednet.edu>
2020-12-07 15:55:35 -0800

    added a chain

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
+//Namespacing
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constrainedLog;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -16,6 +20,7 @@ function setup(){
     world = engine.world;
 
 
+    constrainedLog = new Log(230, 180, 80, PI/2);
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -30,12 +35,15 @@ function setup(){
 
     log3 =  new Log(810,180,300, PI/2);
 
+    
+
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
 
+    chain = new Chain(constrainedLog.body, bird.body);
 }
 
 function draw(){
@@ -61,4 +69,7 @@ function draw(){
 
     bird.display();
     platform.display();
+    constrainedLog.display();
+
+    chain.display();
 }
